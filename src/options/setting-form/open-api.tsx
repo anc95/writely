16d9eb103c9b204e37104f2ca35f9089bd7b7f92
anc95 <1481988258@qq.com>
2023-03-04 14:11:30 +0800

    chore: add test connection

--- src/options/setting-form/open-api.tsx
+++ src/options/setting-form/open-api.tsx
@@ -1,5 +1,6 @@
-import { Form, Input, Select } from 'antd';
-import { useModels } from '../../common/api/openai';
+import { Button, Form, Input, message, Modal, Select, Typography } from 'antd';
+import { useCallback, useState } from 'react';
+import { useModels, useQueryOpenAIPrompt } from '../../common/api/openai';
 import { Block } from './block';
 
 export const OPENAISettings: React.FC = () => {
@@ -8,11 +9,16 @@ export const OPENAISettings: React.FC = () => {
   return (
     <div className="w-[800px]">
       <Block>
-        <div className="grid grid-cols-2 gap-8">
-          <Form.Item name="apiKey" label="OpenAI API Key" required>
+        <div className="grid grid-cols-12 gap-8">
+          <Form.Item
+            className="col-span-5"
+            name="apiKey"
+            label="OpenAI API Key"
+            required
+          >
             <Input />
           </Form.Item>
-          <Form.Item name="model" label="Model">
+          <Form.Item className="col-span-5" name="model" label="Model">
             <Select loading={isLoading}>
               {data?.map((model) => (
                 <Select.Option key={model.id} value={model.id}>
@@ -21,8 +27,51 @@ export const OPENAISettings: React.FC = () => {
               ))}
             </Select>
           </Form.Item>
+          <ConnectionTest />
         </div>
       </Block>
+    </div>
+  );
+};
+
+const ConnectionTest: React.FC = () => {
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const queryOpenAI = useQueryOpenAIPrompt();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [result, setResult] = useState<string>('');
+  const [message, setMessage] = useState<string>('hello');
+
+  const handleOk = useCallback(async () => {
+    setLoading(true);
+    setResult('');
+    try {
+      setResult(await queryOpenAI(message));
+    } catch (e) {
+      setResult(e.toString());
+    } finally {
+      setLoading(false);
+    }
+  }, [queryOpenAI]);
+
+  return (
+    <div>
+      <Button onClick={() => setModalVisible(true)} type="primary">
+        Test
+      </Button>
+      <Modal
+        open={modalVisible}
+        okText="Send message"
+        onCancel={() => setModalVisible(false)}
+        title="Test connection"
+        onOk={handleOk}
+        okButtonProps={{ loading }}
+      >
+        <Input.TextArea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></Input.TextArea>
+        <Typography>{result}</Typography>
+      </Modal>
     </div>
   );
 };
