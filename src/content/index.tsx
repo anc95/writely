66ec5fae7893acf1d720e24e2af32f80a1bd27b8
anc95 <1481988258@qq.com>
2023-03-27 17:42:17 +0800

    chore: add prettier check and optimize the extension load

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,23 +1,21 @@
-import '@webcomponents/webcomponentsjs/webcomponents-bundle';
-import { createRoot } from 'react-dom/client';
-import { App } from './app';
-import './index.css';
-import { tag, conatinerId } from './shadow-dom.js';
-import { initI18n } from '../common/i18n';
-import 'animate.css';
+import '@webcomponents/webcomponentsjs/webcomponents-bundle'
+import { createRoot } from 'react-dom/client'
+import { App } from './app'
+import './index.css'
+import { tag, conatinerId } from './shadow-dom.js'
+import { initI18n } from '../common/i18n'
+import 'animate.css'
 
-const render = () => {
-  window.addEventListener('load', async () => {
-    const container = document.createElement(tag);
-    container.className = 'writly-container';
-    document.documentElement.append(container);
+const render = async () => {
+  const container = document.createElement(tag)
+  container.className = 'writly-container'
+  document.documentElement.append(container)
 
-    await initI18n();
+  await initI18n()
 
-    createRoot(container.shadowRoot.querySelector(`#${conatinerId}`)).render(
-      <App />
-    );
-  });
-};
+  createRoot(container.shadowRoot.querySelector(`#${conatinerId}`)).render(
+    <App />
+  )
+}
 
-render();
+render()
