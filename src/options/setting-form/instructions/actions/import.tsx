6d577995e02f2b06d33da4c7bb3635b3890aaea1
anc95 <1481988258@qq.com>
2023-04-13 00:15:16 +0800

    feature: import instructions

--- src/options/setting-form/instructions/actions/import.tsx
+++ src/options/setting-form/instructions/actions/import.tsx
@@ -1,21 +1,46 @@
-import { Button, Popover, Upload } from 'antd'
+import { batchAdd } from '@/common/api/instructions'
+import { useSettings } from '@/common/store/settings'
+import { Button, message, Popover, Upload } from 'antd'
 import i18next from 'i18next'
-import { useModalState } from '../modal-state'
+import { debounce } from 'lodash-es'
+import { useCallback, useState } from 'react'
 
 export const Import: React.FC = () => {
-  const { setIsOpen } = useModalState()
+  const { refresh } = useSettings()
+  const [loading, setLoading] = useState(false)
+
+  const handleUploadChange = useCallback(
+    debounce(
+      async (file: File) => {
+        try {
+          setLoading(true)
+          const text = await file.text()
+          const json = JSON.parse(text)
+          await batchAdd(json)
+          await refresh()
+          message.success(i18next.t('😄 Imported successfully'))
+        } catch {
+          message.error(i18next.t('😭 Error format'))
+        } finally {
+          setLoading(false)
+        }
+      },
+      1000,
+      { leading: true, trailing: false }
+    ),
+    []
+  )
+
+  console.log(loading)
 
   return (
     <Popover content={i18next.t('Import instructions')}>
       <Upload
-        disabled
         accept=".json"
         showUploadList={false}
-        onChange={async ({ file }) => {
-          console.log(await file.originFileObj.text())
-        }}
+        onChange={({ file }) => handleUploadChange(file.originFileObj)}
       >
-        <Button onClick={() => setIsOpen(true)}>{i18next.t('Import')}</Button>
+        <Button loading={loading}>{i18next.t('Import')}</Button>
       </Upload>
     </Popover>
   )
