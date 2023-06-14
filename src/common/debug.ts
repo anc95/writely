3f417c4ca0abe7600511ea556a2dd25bdb33d205
anc95 <1481988258@qq.com>
2023-06-14 15:06:45 +0800

    fix prompt template remove comma

--- src/common/debug.ts
+++ src/common/debug.ts
@@ -1,27 +1,27 @@
-import { getSetting } from './store/settings';
+import { getSetting } from './store/settings'
 
 class Logger {
-  private _debug = false;
+  private _debug = false
 
   constructor() {
-    this.init();
+    this.init()
   }
 
   private async init() {
-    const settings = await getSetting();
+    const settings = await getSetting()
 
     if (settings.debug) {
-      this._debug = true;
+      this._debug = true
     }
   }
 
   public debug = (...args) => {
     if (this._debug) {
-      console.debug(...args);
+      console.debug(...args)
     }
-  };
+  }
 }
 
-const logger = new Logger();
+const logger = new Logger()
 
-export { logger };
+export { logger }
