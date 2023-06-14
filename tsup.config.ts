662a7639930b01e58723a2c8a8eb0eef266e7b9c
anc95 <1481988258@qq.com>
2023-06-14 17:09:18 +0800

    chore: firefox

--- tsup.config.ts
+++ tsup.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   splitting: false,
   sourcemap: false,
   clean: true,
+  minify: true,
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   },
